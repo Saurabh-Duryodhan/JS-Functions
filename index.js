259efe437e1570f5d8283d4b74b066a72ba1057d
Saurabh R. Duryodhan <srduryodhan97@gmail.com>
2024-01-10 21:30:23 +0530

    console added

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const app = express()
 const apiService = new ProductService()
 app.get('/getProductsByCategories', async (req, res) => {
     const result = await apiService.getGroupedByCategories(apiService.cart)
+    console.log(result)
     res.send(result)
 })
 app.listen(3000, () => console.log(`Listening on port number: 3000`))
